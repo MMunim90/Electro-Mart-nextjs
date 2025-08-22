6fb897efbd4e2860e6d505d82fbae325037b2bed
MMunim <mmunim9.9.01@gmail.com>
2025-08-22 08:24:13 +0600

    Navbar section done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,74 @@
+"use client";
+// import { signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export default function Navbar() {
+//   const session = useSession();
+//   const {data: cookie, status} = session;
+//   console.log(session);
+  const navMenu = () => {
+    return (
+      <>
+        <li>
+          <Link href={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link href={"/products"}>Products</Link>
+        </li>
+      </>
+    );
+  };
+  return (
+    <div className="navbar bg-white shadow-sm px-12">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden text-black">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-white rounded-box z-1 mt-3 w-52 p-2 shadow text-black"
+          >
+            {navMenu()}
+          </ul>
+        </div>
+        <Link href={"/"} className="text-xl">
+          <Image src={"/assets/logo.png"} width={87} height={87} alt="logo" />
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex text-black">
+        <ul className="menu menu-horizontal px-1 text-lg">{navMenu()}</ul>
+      </div>
+      <div className="navbar-end">
+        <ul className="flex mr-4 gap-4 text-lg">
+          {status == "authenticated" ? (<>
+          <li onClick={() => signOut()}  className="btn btn-outline rounded text-indigo-700">
+            Logout
+          </li>
+          <li><Image  className="rounded-full border-3 border-indigo-700" src={cookie?.user?.image} width={40} height={40} alt="user-image"></Image></li>
+          </>) : (<>
+          <li className="btn btn-outline rounded text-indigo-700">
+            <Link href={"/login"}>Login</Link>
+          </li>
+          </>)}
+        </ul>
+      </div>
+    </div>
+  );
+}
